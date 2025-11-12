ee3083db74b60f92fe43bc1042bd8572d35847f6
Rickard Almgren <almgren.rickard96@gmail.com>
2025-11-12 11:32:47 +0100

    changed to php for requires

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer>
+    <div class="footer-grid">
+        <img class="logo" src="images/logo-black.png">
+        <section class="p1">
+            <p><strong>Contact</strong><br>
+                Learningstreet 3<br>
+                417 56 Gothenburg<br>
+
+                bio@deploy.se<br>
+                031-473056<br>
+            </p>
+        </section>
+
+        <section class="p2">
+            <p><strong>Links</strong><br>
+                Buy tickets<br>
+                Movies<br>
+                Our menu<br>
+                Newsletter<br>
+            </p>
+        </section>
+
+        <section class="p3">
+            <p><strong>Socials</strong></p>
+        </section>
+        <section class="grid-logos">
+            <img class="fb" src="images/fb-logo.svg">
+            <img class="ig" src="images/ig-logo.svg">
+            <img class="x" src="images/x-logo.svg">
+            <img class="yt" src="images/yt-logo.svg">
+        </section>
+    </div>
+    <img class="hans" src="images/hans-approves.png">
+    <section class="copyright">
+        &copy; Bio DePloy 2025. All rights reserved.
+    </section>
+</footer>
+
+
+</body>
+
+</html>
---
+++ moviepage.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/components.css">
+    <link rel="stylesheet" href="css/moviepage.css">
+    <link rel="stylesheet" href="css/media.css">
+    <link rel="stylesheet" href="css/offer.css">
+    <link rel="stylesheet" href="css/footer.css">
+
+</head>
+
+<body>
+
+    <header>
+        <img class="logo" src="images/Logga(1).png">
+    </header>
+    <section class="movie-presentation">
+
+        <!-- BACKGROUND IMAGE, MOVIE POSTER, H1 & BUTTON -->
+        <div class="image-container">
+            <h1>Filezilla - King of Monsters</h1>
+            <img class="moviepage-poster" src="images/FILEZILLA.png">
+            <button class="button-info">Get your tickets! <img src="images/button-arrow.png"></button>
+        </div>
+
+    </section>
+
+    <section class="movie-information">
+        <h2>Information</h2>
+        <div class="info-boxes">
+            <div class="info-box">Premiere 2/12</div>
+            <div class="info-box">Horror</div>
+            <div class="info-box">1h 57min</div>
+            <div class="info-box">PG13</div>
+        </div>
+        <p><strong>About:</strong> The world is beset by the appearance of monstrous creatures,
+            but one of them may be the only one who can save humanity.</p>
+        <div class="movie-information-details">
+            <p><strong>Director:</strong> </p>
+            <p><strong>Actors:</strong> </p>
+            <p><strong>Language:</strong> English</p>
+        </div>
+
+    </section>
+
+    <!--GALLERY-->
+    <div class="secondary-background gallery-carousel">
+        <h2>GALLERY</h2>
+        <div class="carousel-container">
+            <img class="carousel-img" src="images/Brokebackend.png">
+            <img class="carousel-img" src="images/GreatGitsby.png">
+            <img class="carousel-img" src="images/drypoet.png">
+        </div>
+    </div>
+
+    <div class="secondary-background" id="gallery-grid">
+        <h2>GALLERY</h2>
+        <div class="gallery-grid-container">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+
+        </div>
+    </div>
+
+    <div class="divider-gold"></div>
+
+    <!-- BOOKING MODULE -->
+    <section class="booking-module">
+        <div class="booking-module-text">
+            <h2>Tickets</h2>
+            <p><strong>Date</strong></p>
+        </div>
+        <div class="date-block-container">
+            <div class="date-block">2/12</div>
+            <div class="date-block" id="date-block-selected">3/12</div>
+            <div class="date-block">4/12</div>
+            <div class="date-block">5/12</div>
+            <div class="date-block">6/12</div>
+            <div class="date-block">7/12</div>
+            <div class="date-block">7/12</div>
+            <div class="date-block" id="more">></div>
+        </div>
+        <div class="booking-module-text">
+            <p><strong>Time</strong></p>
+        </div>
+        <div class="time-block-container">
+            <div class="time-block">10.00</div>
+            <div class="time-block">12.30</div>
+            <div class="time-block">15.00</div>
+            <div class="time-block" id="date-block-selected">17.30</div>
+            <div class="time-block">20.00</div>
+            <div class="time-block">22.30</div>
+        </div>
+        <button class="button-info">Continue<img src="images/button-arrow.png"></button>
+    </section>
+    <div class="divider-gold"></div>
+
+    <!--CAROUSEL-->
+    <div class="secondary-background">
+        <h2>YOU MAY ALSO LIKE</h2>
+        <div class="carousel-container">
+            <img class="carousel-img" src="images/Brokebackend.png">
+            <img class="carousel-img" src="images/GreatGitsby.png">
+            <img class="carousel-img" src="images/drypoet.png">
+        </div>
+    </div>
+
+    <?php
+
+    require __DIR__ . "/footer.php";
