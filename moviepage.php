e18bc51276141d396cb49b5630b19974aca6599f
mariatedeman <m.tedeman@hotmail.com>
2025-11-20 13:13:44 +0100

    #Moviepage desktop started

--- moviepage.php
+++ moviepage.php
@@ -22,9 +22,9 @@
     </header>
 
     <!-- BACKGROUND IMAGE, MOVIE POSTER, H1 & BUTTON -->
-    <section class="movie-presentation">
+    <section class="moviepage-movie-presentation">
         <div class="moviepage-image-container">
-            <h1 class="movie-presentation-h1">Filezilla - King of Monsters</h1>
+            <h1 class="moviepage-movie-presentation-h1">Filezilla - King of Monsters</h1>
             <img class="moviepage-poster" src="images/FILEZILLA.png">
             <button class="button-info">Get your tickets! <img src="images/button-arrow.png"></button>
         </div>
@@ -32,17 +32,17 @@
     </section>
 
 
-    <section class="movie-information">
+    <section class="moviepage-movie-information">
         <h2>Information</h2>
-        <div class="info-boxes">
-            <div class="info-box">Premiere 2/12</div>
-            <div class="info-box">Horror</div>
-            <div class="info-box">1h 57min</div>
-            <div class="info-box">PG13</div>
+        <div class="moviepage-info-boxes">
+            <div class="moviepage-info-box">Premiere 2/12</div>
+            <div class="moviepage-info-box">Horror</div>
+            <div class="moviepage-info-box">1h 57min</div>
+            <div class="moviepage-info-box">PG13</div>
         </div>
         <p><strong>About:</strong> The world is beset by the appearance of monstrous creatures,
             but one of them may be the only one who can save humanity.</p>
-        <div class="movie-information-details">
+        <div class="moviepage-movie-information-details">
             <p><strong>Director:</strong> Rickard Almgren</p>
             <p><strong>Actors:</strong> Tyrannousaurus Flex, Clawdette Wilson, Gittan Hubert, </p>
             <p><strong>Language:</strong> English</p>
@@ -51,31 +51,31 @@
     </section>
 
     <!--GALLERY CAROUSEL-->
-    <div class="secondary-background gallery-carousel">
+    <div class="secondary-background moviepage-gallery-carousel">
         <h2>GALLERY</h2>
         <section class="carousel">
             <div class="card">
-                <img src="">
+                <img src="/images/filezilacrazy.png">
             </div>
 
             <div class="card">
-                <img src="">
+                <img src="/images/filezilawine.png">
             </div>
 
             <div class="card">
-                <img src="">
+                <img src="/images/filezillacute.png">
             </div>
 
             <div class="card">
-                <img src="">
+                <img src="/images/filezillathink.png">
             </div>
         </section>
     </div>
 
     <!-- DESKTOP ONLY: GALLERY GRID -->
-    <div class="secondary-background" id="gallery-grid">
+    <div class="secondary-background" id="moviepage-gallery-grid">
         <h2>GALLERY</h2>
-        <div class="gallery-grid-container">
+        <div class="moviepage-gallery-grid-container">
             <div><img src="/images/filezillathink.png" alt=""></div>
             <div><img src="/images/filezilawine.png" alt=""></div>
             <div><img src="/images/filezillacute.png" alt=""></div>
@@ -86,38 +86,38 @@
     <div class="divider-gold"></div>
 
     <!-- BOOKING MODULE -->
-    <section class="booking-module">
-        <div class="booking-module-text">
+    <section class="moviepage-booking-module">
+        <div class="moviepage-booking-module-text">
             <h2>Tickets</h2>
             <p><strong>Date</strong></p>
         </div>
-        <div class="date-block-container">
-            <div class="date-block">2/12</div>
-            <div class="date-block" id="date-block-selected">3/12</div>
-            <div class="date-block">4/12</div>
-            <div class="date-block">5/12</div>
-            <div class="date-block">6/12</div>
-            <div class="date-block">7/12</div>
-            <div class="date-block">7/12</div>
-            <div class="date-block" id="more">></div>
+        <div class="moviepage-date-block-container">
+            <div class="moviepage-date-block">2/12</div>
+            <div class="moviepage-date-block" id="moviepage-date-block-selected">3/12</div>
+            <div class="moviepage-date-block">4/12</div>
+            <div class="moviepage-date-block">5/12</div>
+            <div class="moviepage-date-block">6/12</div>
+            <div class="moviepage-date-block">7/12</div>
+            <div class="moviepage-date-block">7/12</div>
+            <div class="moviepage-date-block" id="moviepage-booking-module-more">></div>
         </div>
-        <div class="booking-module-text">
+        <div class="moviepage-booking-module-text">
             <p><strong>Time</strong></p>
         </div>
-        <div class="time-block-container">
-            <div class="time-block">10.00</div>
-            <div class="time-block">12.30</div>
-            <div class="time-block">15.00</div>
-            <div class="time-block" id="date-block-selected">17.30</div>
-            <div class="time-block">20.00</div>
-            <div class="time-block">22.30</div>
+        <div class="moviepage-time-block-container">
+            <div class="moviepage-time-block">10.00</div>
+            <div class="moviepage-time-block">12.30</div>
+            <div class="moviepage-time-block">15.00</div>
+            <div class="moviepage-time-block" id="moviepage-date-block-selected">17.30</div>
+            <div class="moviepage-time-block">20.00</div>
+            <div class="moviepage-time-block">22.30</div>
         </div>
         <button class="button-info">Continue<img src="images/button-arrow.png"></button>
     </section>
     <div class="divider-gold"></div>
 
     <!--YOU MAY ALSO LIKE CAROUSEL-->
-    <div class="secondary-background gallery-carousel">
+    <div class="secondary-background">
         <h2>Coming soon!</h2>
         <section class="carousel">
 
