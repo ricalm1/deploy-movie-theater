cea55344ca0e8752df590c40d9f1f04804c10a83
mariatedeman <m.tedeman@hotmail.com>
2025-12-02 11:00:51 +0100

    last fixes

--- index.php
+++ index.php
@@ -1,38 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once __DIR__ . "/header.php" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Deploy theater</title>
-    <link rel="stylesheet" href="css/components.css">
-    <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/moviepage.css">
-    <link rel="stylesheet" href="css/offer.css">
-    <link rel="stylesheet" href="css/media.css">
-
-
-</head>
-
-<body>
-    <header>
-        <img class="logo" src="images/Logga(1).png">
-    </header>
-    <div class="image-container">
-        <img class="hero-img" src="/images/filezilacrazy.png">
-        <div class="heroInfoContainer">
-            <h1 class="title">Filezilla -<br> King of Monsters</h1>
-            <div class="button-container">
-                <a href="moviepage.php" class="button-info">
-                    Get your tickets! <img src="images/button-arrow.png">
-                </a>
-                <button class="button-main">About the movie! <img src="images/button-arrow.png"></button></a>
-            </div>
+<div class="image-container">
+    <img class="hero-img" src="/images/filezilacrazy.png">
+    <div class="heroInfoContainer">
+        <h1 class="title">Filezilla -<br> King of Monsters</h1>
+        <div class="button-container">
+            <a href="moviepage.php" class="button-info">
+                Get your tickets! <img src="images/button-arrow.png">
+            </a>
+            <button class="button-main">About the movie! <img src="images/button-arrow.png"></button></a>
         </div>
     </div>
-    <h3>Watch now! </h3>
+</div>
+
+<!-- CAROUSEL CONTAINER | WATCH NOW -->
+<section class="carousel-container-watchnow">
+
+    <h2>Watch now</h2>
 
     <section class="carousel">
 
@@ -60,7 +44,6 @@
             <img src="/images/GOODWILLCODING.png">
         </div>
 
-
         <div class="card">
             <img src="/images/gitPullfiction.png">
         </div>
@@ -80,41 +63,37 @@
         <div class="card">
             <img src="/images/Undefined.png">
         </div>
-
-
-
     </section>
+</section>
 
 
-    <section class="offer">
-
-
-        <div id="popcorn">
-            <img src="/images/popcorn.png">
-            <div class="popcorn-content">
-                <h2>Don't miss out!</h2>
-                <p>Buy 2 tickets today, get a small popcorn with your choice of seasoning!</p>
-                <button class="button-info">Get your tickets!</button>
-            </div>
-            <p class="popcorn-disclaimer"><small>Normal price 120kr. Valid until midnight.</small></p>
+<section class="offer">
+    <div id="popcorn">
+        <img src="/images/popcorn.png">
+        <div class="popcorn-content">
+            <h2>Don't miss out!</h2>
+            <p>Buy 2 tickets today, get a small popcorn with your choice of seasoning!</p>
+            <button class="button-info">Get your tickets!</button>
         </div>
-
-
-
-    </section>
-    <div class="newsletter-section">
-        <h2>Don't miss out on any news or offers!</h2>
-        <p>Sign up to our news letter for the latest updates and special offers.</p>
-
-        <div class="newsletter-form">
-            <input type="email" placeholder="Your email...">
-            <button class="newsletter-button"><img src="/images/light-arrow.png"></button>
-        </div>
+        <p class="popcorn-disclaimer"><small>Normal price 120kr. Valid until midnight.</small></p>
     </div>
+</section>
+
+<div class="newsletter-section">
+    <h2>Don't miss out on any news or offers!</h2>
+    <p>Sign up to our news letter for the latest updates and special offers.</p>
+
+    <div class="newsletter-form">
+        <input type="email" placeholder="Your email...">
+        <button class="newsletter-button"><img src="/images/light-arrow.svg"></button>
     </div>
+</div>
+</div>
 
-    <h2 class="coming-soon-header">Coming soon!</h2>
+<!-- CAROUSEL CONTAINER | COMING SOON -->
+<section class="carousel-container">
 
+    <h2>Coming soon</h2>
 
     <section class="carousel">
 
@@ -151,11 +130,10 @@
         </div>
 
     </section>
+</section>
 
 
+<?php
 
 
-    <?php
-
-
-    require __DIR__ . "/footer.php";
+require __DIR__ . "/footer.php";
